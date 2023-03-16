76301ebd319f62456e963fc15260cf9a18841c02
sunkeysun <sunkeysun@tencent.com>
2023-03-16 20:48:13 +0800

    feat: 修改默认值

--- src/defaults.ts
+++ src/defaults.ts
@@ -4,10 +4,6 @@
 import type { ApiConfig } from './types.js'
 
 export const defaults: ApiConfig = {
-  timeout: 10000,
-  maxContentLength: 10 * 1024,
-  maxBodyLength: 10 * 1024,
-  withCredentials: true,
   apiService: {
     observe: 'body'
   },
