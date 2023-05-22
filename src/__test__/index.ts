1fe07307ee602039b3eee7da3fb808ac74b9fee8
sunkeysun <sunkeysun@tencent.com>
2023-05-22 09:51:44 +0800

    feat: 修正返回值类型映射

--- src/__test__/index.ts
+++ src/__test__/index.ts
@@ -5,7 +5,7 @@ import { Api, Service, ApiConfig, ApiService } from '../index.js'
 })
 export class UserService extends ApiService {
   @Api({ baseURL: '/hello', url: '/d7389eca-12e9-4e0e-b55e-4704fe7cbfc4' })
-  static getData(data?: unknown, config?: ApiConfig) {
+  static getData(data?: Record<string, string>, config?: ApiConfig) {
     return this.apiCall<Record<string, unknown>>(data, config)
   }
 }
